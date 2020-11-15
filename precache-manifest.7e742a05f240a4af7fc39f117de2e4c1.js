59ae90769dba914ea59f570511418287e237d578
nmpham2 <nmpham2@uci.edu>
2020-11-15 00:15:39 -0800

    Updates

---
+++ precache-manifest.7e742a05f240a4af7fc39f117de2e4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33ff1bfef24ed9ccad706cfab2bf4bc",
+    "url": "/zothacks-2020-frontend/index.html"
+  },
+  {
+    "revision": "38fa5c1008252ef5b462",
+    "url": "/zothacks-2020-frontend/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "d9fa9b5a2c084de15dad",
+    "url": "/zothacks-2020-frontend/static/css/main.0025e172.chunk.css"
+  },
+  {
+    "revision": "38fa5c1008252ef5b462",
+    "url": "/zothacks-2020-frontend/static/js/2.6cc1e2b2.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/zothacks-2020-frontend/static/js/2.6cc1e2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fa9b5a2c084de15dad",
+    "url": "/zothacks-2020-frontend/static/js/main.c6b75443.chunk.js"
+  },
+  {
+    "revision": "f4379ee94c44c9bf53f6",
+    "url": "/zothacks-2020-frontend/static/js/runtime-main.0b10ceb0.js"
+  }
+]);
